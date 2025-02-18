d5628035e55f1b0d56557882e7d59ae2b726aade
최현준 <choijjunii1@gmail.com>
2025-02-18 17:22:21 +0900

    toString변환,parse 사용 JSON 만들기 직전

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 //* 서버 생성
 const http = require('http');
 const fs = require('fs');
+//* 쿼리스트링
+const qs = require('querystring');
 
 //* GET,POST 만들어봄
 //todo form태그 POST랑 연결시도
@@ -12,14 +14,20 @@ const server = http.createServer(function (request, response) {
       response.end(index);
     }
   };
+  const realData = [];
   if (request.method === 'POST') {
     if (request.url === "/practice") {
       request.on('data', function (data) {
-        console.log(data);
-
-
+        realData.push(data);
       });
       request.on('end', function () {
+        //* toString으로 변환후 쿼리스트링 파스로 콘솔찍어봄. 콘솔은 다찍어봄. ㅋㅋ//
+        console.log(realData);
+        const toStringData = realData.toString();
+        console.log(toStringData);
+        const stringData = qs.parse(toStringData);
+        console.log(stringData);
+        
       })
     }
   }
