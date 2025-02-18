e3fc63eb7a21d38b9ab01c4bf065b7589efce06a
최현준 <choijjunii1@gmail.com>
2025-02-18 14:16:43 +0900

    GET,POST만 만들어놓았음

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('issue 연습중')
+//* 서버 생성
+const http = require('http');
+const fs = require('fs');
+
+//* GET,POST 만들어봄
+//todo 메인페이지 인덱스랑 연결해보고 form도 연결해보자
+const server = http.createServer(function (request, response) {
+  if (request.method === 'GET') {
+    
+  };
+  if (request.method === 'POST') {
+    
+  }
+});
+
+server.listen(4000, function () {
+  console.log("Loading... http://localhost:3000/");
+});
