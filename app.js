7280a73dc3fdd1413f4985d39b306364b632f742
최현준 <choijjunii1@gmail.com>
2025-02-18 17:43:09 +0900

    JSON파일 생성 후 데이터 집어넣기

--- app.js
+++ app.js
@@ -22,12 +22,17 @@ const server = http.createServer(function (request, response) {
       });
       request.on('end', function () {
         //* toString으로 변환후 쿼리스트링 파스로 콘솔찍어봄. 콘솔은 다찍어봄. ㅋㅋ//
-        console.log(realData);
         const toStringData = realData.toString();
-        console.log(toStringData);
         const stringData = qs.parse(toStringData);
-        console.log(stringData);
         
+        //* JSON에 데이터를 집어 넣기위한 작업. 
+        const json = JSON.stringify(stringData, null, 2);
+        console.log(json);
+        fs.appendFileSync('data.JSON', json);
+
+        //* JSON을 화면 출력하기 위한 작업들 // 
+        response.writeHead(200, { "Content-Type": "application/json" });
+        response.end(json);
       })
     }
   }
